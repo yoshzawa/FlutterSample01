029cb51f481b9c5199f69662a71c317a5700e743
yoshzawa <yoshzawa@gmail.com>
2022-11-30 18:08:17 +0900

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+
+void main() => runApp(MyApp()); /*1*/
+
+class MyApp extends StatelessWidget {/*2*/
+  @override
+  Widget build(BuildContext context) { /*3*/
+    final wordPair = WordPair.random();
+
+    return MaterialApp(  /*4*/
+      title: 'Welcome to Flutter',
+      home: Scaffold( /*5*/
+        appBar: AppBar( /*6*/
+          title: Text('Welcome to Flutter'),
+        ),
+        body: Center( /*7*/
+          child: Text(wordPair.asPascalCase),
+        ),
+      ),
+    );
+  }
+}
